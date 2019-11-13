aa92031e57c20bc3fc953879b8234d390b5b6d74
Andrey <andrey.melnikov@basetis.com>
2019-11-13 22:37:58 +0100

    [FA-1] Create RegisterActivity and onClick to start activity

---
+++ app/src/main/java/com/example/fichapp/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.fichapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        ImageButton registerButton = findViewById(R.id.register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRegisterActivity();
+            }
+        });
+    }
+    private void startRegisterActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
