f93d9a3a8e6c4d35f590521cb2c8fd9021d70766
Andrey <andrey.melnikov@basetis.com>
2019-11-29 15:17:18 +0100

    [FA-2] Function of navBottomBar

--- app/src/main/java/com/example/fichapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/fichapp/ui/main/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import com.example.fichapp.R;
 import com.example.fichapp.databinding.ActivityMainBinding;
+import com.example.fichapp.ui.history.HistoryFragment;
 import com.example.fichapp.ui.singing.SingInFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel viewModel;
     private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentTransaction transaction;
+    private SingInFragment singInFragment = new SingInFragment();
+    private HistoryFragment historyFragment = new HistoryFragment();
     ImageButton singInButton, userButton, configButton;
 
     @Override
@@ -27,8 +32,32 @@ public class MainActivity extends AppCompatActivity {
         userButton = findViewById(R.id.user_button);
         configButton = findViewById(R.id.config_button);
 
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment, new SingInFragment(),"SingInFragment");
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment, singInFragment);
+        transaction.commit();
+        userButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startHistory();
+            }
+        });
+        singInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startSingIn();
+            }
+        });
+    }
+
+    private void startSingIn(){
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment, singInFragment);
+        transaction.commit();
+    }
+
+    private void startHistory(){
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment, historyFragment);
         transaction.commit();
     }
 }
