3295e9a1e111cb4cd596bc9977c9d1022a7b090d
Andrey <andrey.melnikov@basetis.com>
2019-12-02 18:41:19 +0100

    [FA-4] Add ViewGone to stop button

--- app/src/main/java/com/example/fichapp/ui/singing/SingInFragment.java
+++ app/src/main/java/com/example/fichapp/ui/singing/SingInFragment.java
@@ -1,19 +1,28 @@
 package com.example.fichapp.ui.singing;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.fichapp.R;
 import com.example.fichapp.databinding.FragmentSingInBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class SingInFragment extends Fragment {
 
     private SingInViewModel viewModel;
+    FloatingActionButton signInOut, stopWork;
 
     @Override
     public View onCreateView(
@@ -27,4 +36,20 @@ public class SingInFragment extends Fragment {
         binding.setViewModel(viewModel);
         return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        signInOut = Objects.requireNonNull(getView()).findViewById(R.id.signInOut);
+        stopWork = getView().findViewById(R.id.stopWork);
+        signInOut.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onClick(View v) {
+                stopWork.setVisibility(View.VISIBLE);
+                signInOut.setImageResource(R.drawable.ic_simbolo_de_pausa);
+            }
+        });
+    }
+
 }
